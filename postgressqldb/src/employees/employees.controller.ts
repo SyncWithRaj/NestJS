801fb9104a831a664a593afce744394540476342
MrCoderRaj <rajr127655@gmail.com>
2025-11-10 22:21:29 +0530

    GET Api in postgresSQL

--- postgressqldb/src/employees/employees.controller.ts
+++ postgressqldb/src/employees/employees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Employee } from './employees.entity';
 import { Repository } from 'typeorm';
@@ -8,11 +8,21 @@ export class EmployeesController {
   constructor(
     @InjectRepository(Employee)
     private readonly employeeRepository: Repository<Employee>,
-  ) {}
+  ) { }
 
   @Post()
   async create(@Body() employeeData: Partial<Employee>): Promise<Employee> {
     const employee = this.employeeRepository.create(employeeData);
     return await this.employeeRepository.save(employee);
+  }
+
+  @Get()
+  async findAll(): Promise<Employee[]> {
+    return this.employeeRepository.find();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<Employee | null> {
+    return this.employeeRepository.findOne({ where: { id } });
   }
 }
